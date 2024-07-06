9b2485377c0f0dec081d76a13016bf9cac355ba9
Amy <bergstromaa@gmail.com>
2024-07-06 07:48:38 +0100

    add function to calculate average of last 5 stock levels, add 10% and round

--- run.py
+++ run.py
@@ -83,6 +83,41 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    pprint(columns)
+
+    return columns
+
+def calculate_stock_data(data):
+    """
+    Calculate average stock for each sandwich type.
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return(new_stock_data)
+
+
+
+
 def main():
     """
     Run all program functions
@@ -96,4 +131,8 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
+stock_data = calculate_stock_data(sales_columns)
+print(stock_data)
